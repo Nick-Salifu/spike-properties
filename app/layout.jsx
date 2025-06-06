fbbb10e820f54276ea05a251c3693f9c58b3e77a
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-06-06 02:05:03 +0100

    starting spike properties project

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  display: 'swap'
+})
+
+export const metadata = {
+  title: "Spike Properties",
+  keywords: "rentals, property, real estate",
+  description: "Find the perfect rental property",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={poppins.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Home Page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit.</p>
+    </div>
+  );
+}
