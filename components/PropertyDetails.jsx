9460d4d0162b652a307fc01a566b23e630e42ad9
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-06-30 04:20:23 +0100

    spacing

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -48,16 +48,16 @@ const PropertyDetails = ({property}) => {
               <h3 className="text-lg font-bold mb-6">Description & Details</h3>
               <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
                 <p>
-                  <FaBed className="inline-block mr-2" /> {property.beds}
+                  <FaBed className="inline-block mr-2" /> {property.beds}{' '}
                   <span className="hidden sm:inline">Beds</span>
                 </p>
                 <p>
-                  <FaBath className="inline-block mr-2" /> {property.baths}
+                  <FaBath className="inline-block mr-2" /> {property.baths}{' '}
                   <span className="hidden sm:inline">Baths</span>
                 </p>
                 <p>
                   <FaRulerCombined className="inline-block mr-2" />
-                  {property.square_feet} <span className="hidden sm:inline">sqft</span>
+                  {property.square_feet}{' '} <span className="hidden sm:inline">sqft</span>
                 </p>
               </div>
               <p className="text-gray-500 mb-4">
