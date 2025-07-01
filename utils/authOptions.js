690884a5a3cf120817a32f60038ae3665a85867f
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-07-01 23:09:35 +0100

    creating the next auth provider

---
+++ utils/authOptions.js
@@ -0,0 +1,25 @@
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: 'consent',
+                    access_type: 'offline',
+                    response_type: 'code'
+                }
+            }
+        })
+    ],
+    callbacks: {
+        // Invoked on successful sign in
+        async signIn({ profile }) {
+            
+        },
+        // session callback function
+        async session({ session }) {
+            
+        }
+    }
+}
