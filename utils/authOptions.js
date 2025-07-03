24ef15c311f3344652e2568eebb140478437aa9b
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-07-03 01:35:20 +0100

    implementing the auth signIn

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import GoogleProvider from 'next-auth/provider/google'
+
+
 export const authOptions = {
     providers: [
         GoogleProvider({
@@ -15,11 +18,16 @@ export const authOptions = {
     callbacks: {
         // Invoked on successful sign in
         async signIn({ profile }) {
-            
+            // 1. Connect to database
+            // 2. Check if user exists
+            // 3. If not, create user
+            // 4. Return true to allow sign in
         },
-        // session callback function
+        // session callback function that modifies the session object
         async session({ session }) {
-            
+            // 1. get the user from database
+            // 2. Assign user id from the session
+            // 3. Return session
         }
     }
 }
