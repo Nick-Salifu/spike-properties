d5be1d8734ba8372f05c1a54e8f0cb4188e00d23
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-07-08 07:29:05 +0100

    working on the add form page and the server actions corresponding to it

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import PropertyAddForm from "@/components/PropertyAddForm"
 
 const AddPropertyPage = () => {
   return (
-    <div>Add Page</div>
+    <section className="bg-blue-50">
+      <div className="container mx-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 rounded-md shadow-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
