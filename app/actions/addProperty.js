576fb0f76dcf4cb6b0930be5001579f65b6dc23d
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-07-12 07:59:52 +0100

    adding images from cloudinary

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -5,6 +5,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
 async function addProperty(formData) {
     await connectDB;
@@ -22,7 +23,6 @@ async function addProperty(formData) {
     const amenities = formData.getAll('amenities');
     const images = formData.getAll('images')
         .filter((image) => image.name !== '')
-        .map((image) => image.name)
     
     const propertyData = {
         owner: userId,
@@ -49,8 +49,28 @@ async function addProperty(formData) {
             email: formData.get('seller_info.email'),
             phone: formData.get('seller_info.phone'),
         },
-        images,
+    };
+    const imageUrls = [];
+
+    for (const imageFile of images) {
+        const imageBuffer = await imageFile.arrayBuffer();
+        const imageArray = Array.from(new Uint8Array(imageBuffer));
+        const imageData = Buffer.from(imageArray);
+
+        // convert to base64
+        const imageBase64 = imageData.toString('base64');
+
+        // Make request to cloudinary
+        const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+            folder: 'spikeproperty'
+        });
+
+        imageUrls.push(result.secure_url);
     }
+
+    propertyData.images = imageUrls;
+
+
     const newProperty = new Property(propertyData);
     await newProperty.save();
 
--- app/error.jsx
+++ app/error.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { FaExclamationCircle } from 'react-icons/fa'
 
