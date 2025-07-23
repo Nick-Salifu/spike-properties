749dff0bf8d9dd382a46aae1c64b9a070f5bf68c
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-07-23 13:21:12 +0100

    implementing pagination

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,16 @@
+import Pagination from '@/components/Pagination';
 import PropertyCard from '@/components/PropertyCard'
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
 
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({searchParams: {page = 1, pageSize = 9}}) => {
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const skip = (page - 1) * pageSize;
+  const total = await Property.countDocuments({});
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+  const showPagination = total > pageSize;
 
   return (
     <section className='px-4 py-6'>
@@ -20,6 +25,13 @@ const PropertiesPage = async () => {
                 ))
               }
          </div>   
+        )}
+        {showPagination && (
+          <Pagination
+          page={parseInt(page)}
+          pageSize={parseInt(pageSize)}
+          totalItems={total}
+        />
         )}
       </div>
     </section>
