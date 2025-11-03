d7c353e5ffac0cb2a0ae715d2777ccafb0a72f24
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-11-03 01:51:52 +0100

    added event handler to the mobile login button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -210,10 +210,17 @@ const Navbar = () => {
                 </Link>
                 )}
               {!session && (
-                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-                  <i className="fa-brands fa-google mr-2"></i>
-                  <span>Login or Register</span>
-                </button>
+                <div className="flex items-center">
+                  {providers && Object.values(providers).map((provider, index) => (
+                    <button
+                      onClick={() => signIn(provider.id)}
+                      key={index}
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                      <FaGoogle className='text-white mr-2' />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+                </div>
                 )}
             </div>
         </div>
